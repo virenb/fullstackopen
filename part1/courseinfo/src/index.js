25ae4ebce6b24dc7299abe553084ab9f82ad098a
virenb <virenb@gmail.com>
2020-10-01 11:26:49 -0400

    Submission for exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,19 +5,21 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   );
 };
 
@@ -39,10 +41,10 @@ const App = () => {
       <Header course={course} />
       <Content
         part1={part1}
-        part2={part2}
-        part3={part3}
         exercises1={exercises1}
+        part2={part2}
         exercises2={exercises2}
+        part3={part3}
         exercises3={exercises3}
       />
       <Total exercises={exercises1 + exercises2 + exercises3} />
