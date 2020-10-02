812c5aa73d9975f6b5a7405c9b654cbf3477ff0e
virenb <virenb@gmail.com>
2020-10-02 13:38:12 -0400

    Submission for 2.2

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -3,13 +3,21 @@ import Part from './Part';
 
 const Content = ({ parts }) => {
   return (
-    <div>
-      {parts.map((part, id) => {
-        return (
-          <Part key={part.id} name={part.name} exercise={part.exercises} />
-        );
-      })}
-    </div>
+    <>
+      <div>
+        {parts.map((part, id) => {
+          return (
+            <Part key={part.id} name={part.name} exercise={part.exercises} />
+          );
+        })}
+      </div>
+      <div>
+        <strong>
+          total of {parts.map((a) => a['exercises']).reduce((a, b) => a + b)}{' '}
+          exercises
+        </strong>
+      </div>
+    </>
   );
 };
 
