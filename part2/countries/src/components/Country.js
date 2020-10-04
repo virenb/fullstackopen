d17d36c683165479993e497d1ce5a858dec0b5b0
virenb <virenb@gmail.com>
2020-10-04 18:27:30 -0400

    Submission for 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+require('dotenv').config();
+
+const api_key = process.env.REACT_APP_API_KEY;
 
 const Country = ({ country }) => {
+  const [hasWeather, setHasWeather] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+        setHasWeather(true);
+      });
+  }, []);
+
   return (
     <>
       <div>
@@ -19,6 +37,24 @@ const Country = ({ country }) => {
           width='100'
           height='75'
         />
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          {hasWeather ? (
+            <div>
+              <div>temperature {weather.current.temperature} Celsius</div>
+              <img
+                src={weather.current.weather_icons[0]}
+                alt='hi'
+                width='100'
+                height='75'
+              />
+              <div>
+                wind: {weather.current.wind_speed} mph{' '}
+                {weather.current.wind_dir}
+              </div>
+            </div>
+          ) : null}
+        </div>
       </div>
     </>
   );
