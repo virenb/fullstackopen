a61c060f3d236ed1a20eaeaa8fb49bfef7c9f948
virenb <virenb@gmail.com>
2020-10-04 20:56:55 -0400

    Submission 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -40,6 +40,9 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then((response) => console.log(response));
       setPersons(persons.concat(personObject));
     }
     setNewName('');
