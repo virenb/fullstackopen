32afc2361c9e31932062f69ff3d0936fffe7edee
virenb <virenb@gmail.com>
2020-10-08 14:23:09 -0400

    Submission for 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -56,10 +56,16 @@ app.get('/info', (request, response) => {
   <div>${Date()}</div>`);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -84,6 +90,18 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
   });
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
