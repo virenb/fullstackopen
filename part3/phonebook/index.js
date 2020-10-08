92414ba54a95a39234ee6584a22e8005fbd011c5
virenb <virenb@gmail.com>
2020-10-08 14:11:24 -0400

    Submission for exercise 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -80,10 +80,9 @@ app.post('/api/persons', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id).then((result) => {
+    response.status(204).end();
+  });
 });
 
 const PORT = process.env.PORT;
