fcfde1350e028acc9b53e09d86cc9883df695fd2
virenb <virenb@gmail.com>
2020-10-12 20:45:39 -0400

    Exercise 4.17

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,8 +3,9 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
+  const users = await User
+    .find({}).populate('blogs', { title: 1, author: 1, url: 1 })
+  response.json(users.map(u => u.toJSON()))
 })
 
 usersRouter.post('/', async (request, response, next) => {
