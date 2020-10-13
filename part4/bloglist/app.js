5fb2b691d3c88fe0322dd0a0cddbb658145f350b
virenb <virenb@gmail.com>
2020-10-12 21:13:19 -0400

    Exercise 4.18 & 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
