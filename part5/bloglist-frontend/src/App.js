de1afcd32050666118af603f06471b9ddbbe5709
virenb <virenb@gmail.com>
2020-10-16 22:40:24 -0400

    5.9 exercise

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -114,7 +114,9 @@ const App = () => {
           </div>
         )}
       {user !== null
-        ? blogs.map((blog) => <Blog key={blog.id} blog={blog} user={user} addLike={addLike} />)
+        ? blogs
+          .sort((a, b) => (a.likes > b.likes ? -1 : 1))
+          .map((blog) => <Blog key={blog.id} blog={blog} user={user} addLike={addLike} />)
         : null}
     </div>
   );
