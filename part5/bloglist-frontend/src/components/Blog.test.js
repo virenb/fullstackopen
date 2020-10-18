ce42ee48769312b747774321021f09061ccf98bd
virenb <virenb@gmail.com>
2020-10-17 21:01:34 -0400

    exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
 test('renders author and title only by default', () => {
@@ -23,4 +23,33 @@ test('renders author and title only by default', () => {
     'react-testing.org',
     '420',
   );
+});
+
+test('clicking the button shows a blogs likes and url', () => {
+  const blog1 = {
+    title: 'React Testing Library is Fun',
+    author: 'React Tester',
+    url: 'react-testing.org',
+    likes: 421,
+    user: {
+      name: 'Bob Bob',
+      username: 'bob123',
+    },
+  };
+
+  const user = {
+    name: 'Bob Bob',
+    username: 'bob123',
+  };
+
+  const mockHandler = jest.fn();
+
+  const component = render(
+    <Blog blog={blog1} displayMore={mockHandler} user={user} />
+  );
+
+  const button = component.getByText('view');
+  fireEvent.click(button);
+
+  expect(component.container).toHaveTextContent('react-testing.org', '421');
 });
