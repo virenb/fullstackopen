2d72aaccc0bb63b2c1b81cc2d6cefba09811b201
virenb <virenb@gmail.com>
2020-10-23 17:36:23 -0400

    exercise 7.2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -18,10 +18,28 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`} >{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id
+  const anecdote = anecdotes.find(a => a.id === id)
+  
+  return (
+    <div>
+      <h2>{`${anecdote.title} by ${anecdote.author}`}</h2>
+      <p>has {anecdote.votes} votes</p>
+    </div>
+  )
+}
+  
+
 
 const About = () => (
   <div>
@@ -134,6 +152,9 @@ const App = () => {
           </Route>
           <Route path='/create'>
             <CreateNew addNew={addNew} />
+          </Route>
+          <Route path="/anecdotes/:id">
+            <Anecdote anecdotes={anecdotes} />
           </Route>
           <Route path='/'>
             <AnecdoteList anecdotes={anecdotes} />
